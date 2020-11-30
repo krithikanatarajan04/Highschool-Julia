d54f16c400d948f5fe5a023ca05cb06ef39484c4
EC2 Default User <ec2-user@ip-172-31-6-119.us-east-2.compute.internal>
2020-11-30 21:26:58 +0000

    First commit

---
+++ intro.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.12.11
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 078beb8e-3003-11eb-2024-832e809585c7
+md"The goal of this notebook is to simultaneously learn how to program with julia while learning the material for AP Calculus, AP Chemistry, and computer science. In each section contains another notebook where all the notebooks for each subject can be accessed. To reach this, click on the linked here below. "
+
+# ╔═╡ 12f4af38-3003-11eb-192f-6f16e8048709
+md"Calculus notebooks are [here](/open?path=/julia/src/run/notebooks/calculus/intro.jl)"
+
+# ╔═╡ 3c91c06a-3003-11eb-2eae-07b252c6c016
+md"Chemistry notebooks are [here](/open?path=/julia/src/run/notebooks/chemistry/intro.jl)"
+
+# ╔═╡ 49a7f12a-3003-11eb-01c7-efc80a813ddb
+md"Computer Science notebooks are [here](/open?path=/julia/src/run/notebooks/julia/intro.jl)"
+
+# ╔═╡ 8dfe479c-3004-11eb-3d02-3f83cb926b36
+md"Add a new notebook [here](/new)"
+
+# ╔═╡ 3e3458e0-31e5-11eb-1c5d-6bf93bc26648
+🍕 = 1
+
+# ╔═╡ Cell order:
+# ╠═078beb8e-3003-11eb-2024-832e809585c7
+# ╠═12f4af38-3003-11eb-192f-6f16e8048709
+# ╠═3c91c06a-3003-11eb-2eae-07b252c6c016
+# ╠═49a7f12a-3003-11eb-01c7-efc80a813ddb
+# ╠═8dfe479c-3004-11eb-3d02-3f83cb926b36
+# ╠═3e3458e0-31e5-11eb-1c5d-6bf93bc26648
